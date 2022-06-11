59e5175703356c5286b3ac73e50cfdc84f96dd1d
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-06-11 12:51:50 +0300

    set up the project

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/globals.css';
+
+import Layouts from '../components/Layouts';
+
+function MyApp({ Component, pageProps }) {
+  return(
+   
+      
+      <Component {...pageProps} />
+      
+    
+  )
+  
+}
+
+export default MyApp

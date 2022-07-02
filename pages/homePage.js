3f72b066df784ba0f436557b5b1a465e17ccb765
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-02 10:38:29 +0300

    removing Elist errors

--- pages/homePage.js
+++ pages/homePage.js
@@ -20,10 +20,10 @@ function homePage() {
                         Hello, I am <span className='text-indigo-800'>Alphonce</span>
                     </h1>
                         <p className='text-left font-normal mb-5 flex-wrap'>
-                        I am a Software engineer in the field of software development(Full stack) doing both websites and mobile applications.I've got intesive skills on different front-end packages(Reactjs,Angular, .NET) & back-end as well(Python-django, C#) also databases(Postgressql,MySQL ). 
+                        I am a Software engineer in the field of software development(Full stack) doing both websites and mobile applications.I have got intesive skills on different front-end packages(Reactjs,Angular, .NET) & back-end as well(Python-django, C#) also databases(Postgressql,MySQL ). 
                          tudied in Moringa School plus a lot of self commitments and researches.
                         <b/>
-                        I'm Currently working on WEB 3.0 knowledge and projects and I welcome you all to work with me to improve Tech industries. 
+                        I am Currently working on WEB 3.0 knowledge and projects and I welcome you all to work with me to improve Tech industries. 
                         </p>
                         <a href='https://github.com/alphonce-otieno-odhiambo' className='font-semibold text-white md:mt-10 mt-5 pt-5 bg-indigo-900 rounded-md w-60 h-16 text-lg hover:bg-black'>
                             see my GitHub page!
